72fc7fb492cdc7f570a8fc60e10296c231e7d40f
Yash Amin <yashamin26@gmail.com>
2021-12-16 21:17:03 +0530

    logging and progress bar added

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib
 import os
 from matplotlib.colors import ListedColormap
+import logging
 
 plt.style.use('fivethirtyeight')
 
@@ -16,18 +17,33 @@ def prepare_data(df):
     Returns:
         tuple: returns tuples of dependent and independent variables
     """
+    logging.info("preparing the data by segregating dependent and independent variables")
     x = df.drop('y', axis=1)
     y = df['y']
     return x,y
 
 def save_model(model, filename):
+    """this saves the trained model
+
+    Args:
+        model (python object): trained model
+        filename (str): path to save trained model
+    """
+    logging.info("saving the trained model")
     model_dir = 'models'
     os.makedirs(model_dir, exist_ok=True)
     filePath = os.path.join(model_dir, filename)
     joblib.dump(model, filePath)
+    logging.info(f"saved the trained model at {filePath}")
 
 def save_plot(df, file_name, model):
+    """
+    :param df: dataframe
+    :param file_name: path to save plot
+    :param model: trained model
+    """
     def _create_base_plot(df):
+        logging.info("Creating base plot")
         df.plot(kind='scatter', x='x1', y='x2', c='y', s=100, cmap='winter')
         plt.axhline(y=0, color='black', linestyle='--', linewidth=1)
         plt.axvline(x=0, color='black', linestyle='--', linewidth=1)
@@ -35,6 +51,7 @@ def save_plot(df, file_name, model):
         figure.set_size_inches(10,8)
     
     def _plot_decision_region(x,y,classfier,resolution=.02):
+        logging.info("Plotting decision regions")
         colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
         cmap = ListedColormap(colors[: len(np.unique(y))])
         
@@ -45,8 +62,6 @@ def save_plot(df, file_name, model):
         x2_min, x2_max = x2.min() - 1, x2.max() + 1
         
         xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-        print(xx1)
-        print(xx1.ravel())
         
         z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
         z = z.reshape(xx1.shape)
@@ -65,3 +80,4 @@ def save_plot(df, file_name, model):
     os.makedirs(plot_dir, exist_ok=True)
     plotPath = os.path.join(plot_dir, file_name)
     plt.savefig(plotPath)
+    logging.info(f"saving the plot at {plotPath}")
--- utils/model.py
+++ utils/model.py
@@ -1,9 +1,11 @@
 import numpy as np
+import logging
+from tqdm import tqdm
 
 class Perceptron:
     def __init__(self, eta, epochs):
         self.weights = np.random.randn(3) * 1e-4
-        print(f'initial weights before training: {self.weights}')
+        logging.info(f'initial weights before training: {self.weights}')
         self.eta = eta
         self.epochs = epochs
     
@@ -16,22 +18,22 @@ class Perceptron:
         self.y = y
         
         x_with_bias = np.c_[self.x, -np.ones((len(self.x), 1))]
-        print(f'x with bias value: {x_with_bias}')
+        logging.info(f'x with bias value: {x_with_bias}')
         
-        for epoch in range(self.epochs):
-            print('--'*10)
-            print(f'For epoch: {epoch}')
-            print('--'*10)
+        for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
+            logging.info('--'*10)
+            logging.info(f'For epoch: {epoch}')
+            logging.info('--'*10)
             
             y_hat = self.activationFunction(x_with_bias, self.weights) # forward propagation 
-            print(f'predicted value after forward pass: {y_hat}')
+            logging.info(f'predicted value after forward pass: {y_hat}')
             
             self.error = self.y - y_hat
-            print(f'error:\n{self.error}')
+            logging.info(f'error:\n{self.error}')
             
             self.weights = self.weights + self.eta * np.dot(x_with_bias.T, self.error) # backward propagation
-            print(f'updated weights after epoch {epoch}/{self.epochs}: {self.weights}')
-            print('####'*10)
+            logging.info(f'updated weights after epoch {epoch}/{self.epochs}: {self.weights}')
+            logging.info('####'*10)
     
     def predict(self, x):
         x_with_bias = np.c_[x, -np.ones((len(x), 1))]
@@ -39,5 +41,5 @@ class Perceptron:
     
     def total_loss(self):
         total_loss = np.sum(self.error)
-        print(f'total loss: {total_loss}')
+        logging.info(f'total loss: {total_loss}')
         return total_loss
